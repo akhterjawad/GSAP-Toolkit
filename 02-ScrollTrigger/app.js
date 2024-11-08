0d8b95021f2a01c504ce52512f389cf7eb8c2114
Muhammad Jawad <akhterjawad566@gmail.com>
2024-11-08 17:56:03 +0500

    scrolltrger complte

--- 02-ScrollTrigger/app.js
+++ 02-ScrollTrigger/app.js
@@ -1,29 +1,39 @@
 gsap.from(".pdiv1 .box", {
     scale: 0,
     rotate: 360,
-    delay: 2,
+    delay: 1,
     duration: 1.5
 })
-gsap.from(".pdiv2 .box", {
-    scale: 0,
-    rotate: 360,
-    // delay:2,
-    duration: 1.5,
+gsap.from(".pdiv2 h1", {
+    opacity: 0,
+    x: 500,
+    duration: 1,
     scrollTrigger: {
-        trigger: ".pdiv2 .box", //madlab jis cheez pa animation chalani ha usko point karna
+        trigger: ".pdiv2 h1", //madlab jis cheez pa animation chalani ha usko point karna
         scroller: "body",
-        markers: 'true', // markers sa developer ko asani hoti ha ka animation Execute kb karwana ha
-        start: "top 60%" // markers ko uper nicha karna ka lia
+        // markers: 'true', // markers sa developer ko asani hoti ha ka animation Execute kb karwana ha
+        start: "top 50%" // markers ko uper nicha karna ka lia
+    }
+})
+gsap.from(".pdiv2 h2", {
+    opacity: 0,
+    x: -500,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".pdiv2 h1", //madlab jis cheez pa animation chalani ha usko point karna
+        scroller: "body",
+        // markers: 'true', // markers sa developer ko asani hoti ha ka animation Execute kb karwana ha
+        start: "top 50%" // markers ko uper nicha karna ka lia
     }
 })
 gsap.from(".pdiv3 .box", {
     scale: 0,
-    rotate: 360,
+    rotate: -360,
     // delay:2,
     duration: 1.5, scrollTrigger: {
         trigger: ".pdiv3 .box", //madlab jis cheez pa animation chalani ha usko point karna
         scroller: "body",
-        markers: 'true', // markers sa developer ko asani hoti ha ka animation Execute kb karwana ha
+        // markers: 'true', // markers sa developer ko asani hoti ha ka animation Execute kb karwana ha
         start: "top 60%" // markers ko uper nicha karna ka lia
     }
 })
